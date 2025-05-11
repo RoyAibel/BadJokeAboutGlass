f7f472d5358694d547ed30c5ef01058dceba8468
Grady B <broggl@broggl.farm>
2025-05-12 00:24:45 +1000

    flake does some heinous shit now

--- mrbroggle/flake.nix
+++ mrbroggle/flake.nix
@@ -1,27 +1,56 @@
 {
-  description = "A Nix system for someone who drinks water";
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
+
   outputs =
-    {
-      nixpkgs,
-    }:
-    let
-      system = "x86_64-linux";
-    in
-    {
-      nixosConfigurations."Water Drinker" = nixpkgs.lib.nixosSystem {
+    { nixpkgs, ... }@inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system: rec {
+      nixosConfigurations.water = nixpkgs.lib.nixosSystem {
         inherit system;
+        specialArgs.inputs = inputs;
         modules = [
-          {
-            programs.zsh = {
-              enable = true;
-              shellInit = "while true; do echo 'Drinking water'; done;";
-            };
-            users.defaultUserShell = nixpkgs.zsh;
-          }
+          (
+            { pkgs, ... }:
+            {
+              users.groups.admin = { };
+              users.users = {
+                water = {
+                  isNormalUser = true;
+                  extraGroups = [ "wheel" ];
+                  password = "admin";
+                  group = "admin";
+                };
+              };
+
+              virtualisation.vmVariant = {
+                virtualisation = {
+                  memorySize = 2048;
+                  cores = 1;
+                  graphics = true;
+                };
+              };
+              services.cage = {
+                enable = true;
+                user = "water";
+                program = "${pkgs.alacritty}/bin/alacritty -e ${pkgs.bash}/bin/bash -c 'while true; do echo \"DRINK WATER\"; done;'";
+              };
+
+              system.stateVersion = "23.05";
+            }
+          )
         ];
       };
-    };
+      apps = {
+        default = {
+          type = "app";
+          program = "${nixosConfigurations.water.config.system.build.vm}/bin/run-nixos-vm";
+        };
+      };
+    });
 }
